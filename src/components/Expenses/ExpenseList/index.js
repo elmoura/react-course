c9500bc24053e029ba17222cad668caa6e972116
Gabriel Moura <mourasb.gabriel@gmail.com>
2022-11-28 17:00:20 -0300

    feat: add optional text in case there is no filtered expenses available

--- src/components/Expenses/ExpenseList/index.js
+++ src/components/Expenses/ExpenseList/index.js
@@ -8,9 +8,26 @@ export const ExpenseList = ({ items }) => {
   const defaultYear = new Date().getFullYear.toString();
   const [selectedYear, setSelectedYear] = useState(defaultYear);
 
+  const filteredExpenses = items.filter(
+    (expense) => expense.date.getFullYear() === Number(selectedYear)
+  );
+
   const handleYearSelection = (year) => {
     setSelectedYear(year);
   };
+
+  let expensesContent = <p>No expenses found</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
 
   return (
     <div>
@@ -19,14 +36,7 @@ export const ExpenseList = ({ items }) => {
           selectedYear={selectedYear}
           onSelectYear={handleYearSelection}
         />
-        {items.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {expensesContent}
       </Card>
     </div>
   );
